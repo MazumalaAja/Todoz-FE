62e2a9db1fa6488be7a256740cbfc68a070e8aa2
MazumalaAja <programlia1108@gmail.com>
2026-02-02 07:09:00 +0700

    create UI Home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,9 +12,13 @@ export default function HomePage() {
      const [selected, setSelected] = useState({
           year: new Date().getFullYear(),
           month: new Date().getMonth(),
+          date: new Date().getDate() || 1
      });
 
-     // ===== Day in this Month =====
+     // ===== Data Time =====
+     const refrences = useRef(null);
+     const getDayInMonth = new Date(selected.year, selected.month + 1, 0).getDate();
+     const days = Array.from({ length: getDayInMonth }, (_, i) => i + 1);
      const month = Array.from({ length: 12 }, (_, i) => new Date(2026, i).toLocaleString("id-ID", { month: "long" }));
 
      // ===== Data Navigation =====
@@ -26,19 +30,34 @@ export default function HomePage() {
      ]
 
      // ===== HandleClick =====
+     function handleClickDate(value) {
+          setSelected(prev => ({ ...prev, date: value }));
+     }
+
      function handleClickMonth(value) {
-          setSelected(prev => ({ ...prev, month: value.index }));
+          setSelected(prev => ({ ...prev, month: value.index, date: 1 }));
      }
 
      // ===== HandleChangeYear =====
      function handleChangeYear(e) {
           const value = Number(e.target.value);
-          setSelected(prev => ({ ...prev, year: Number(value) }));
+          setSelected(prev => ({ ...prev, year: Number(value), date: 1 }));
+     }
+
+     // ===== HandleChangeYear =====
+     function handleChangeDate(e) {
+          const value = Number(e.target.value);
+          if (value == "" || value < 0 || value > getDayInMonth) return setSelected(prev => ({ ...prev, date: 1 }));
+          setSelected(prev => ({ ...prev, date: Number(value) }));
      }
 
      useEffect(() => {
-          console.log("Bulan : " + selected.month, "Tahun : " + selected.year);
-     }, [selected.month, selected.year])
+          if (refrences.current) {
+               const index = selected.date - 1;
+               const activeDay = refrences.current?.children;
+               activeDay[index].scrollIntoView({ behavior: "smooth", inline: "start" })
+          }
+     }, [selected.month, selected.year, selected.date])
      return (
           <>
                {/* ===== Navbar ===== */}
@@ -46,7 +65,12 @@ export default function HomePage() {
 
                {/* ===== List Tasks ===== */}
                <div className={`bg-neutral-900 min-h-screen py-20 space-y-3`}>
-                    <div className="flex items-center gap-2">
+                    <div className="w-full max-w-6xl mx-auto flex items-center gap-2">
+                         {/* ===== Handle Day ===== */}
+                         <div className="border-neutral-400 border w-max px-3 py-1 rounded-md text-neutral-200">
+                              <input onChange={handleChangeDate} value={selected.date} type="text" placeholder="Choose your own..." className="focus:outline-0" min={1} max={getDayInMonth} />
+                         </div>
+
                          {/* ===== Handle Month ===== */}
                          <div>
                               <Select onClick={handleClickMonth} selected={selected.month} value={new Date(selected.year, selected.month).toLocaleString("id-ID", { month: "long" })} data={month} />
@@ -58,6 +82,14 @@ export default function HomePage() {
                          </div>
                     </div>
 
+                    {/* ===== List Date ===== */}
+                    <div ref={refrences} className="flex items-center gap-3 overflow-x-auto mx-auto w-full max-w-6xl">
+                         {days && days.map((v, i) => (
+                              <CategoryDay onClick={() => handleClickDate(v)} selected={selected.date == i + 1} key={i} month={new Date(selected.year, selected.month).toLocaleString("id-ID", { month: "short" })} label={v} />
+                         ))}
+                    </div>
+
+                    {/* ===== List Tasks ===== */}
                     <div className="w-full max-w-6xl mx-auto">
                          {dataTasks && dataTasks.map((v, i) => (
                               <TasksPlace key={i} label={v.title} date={v.updatedAt} />
@@ -68,11 +100,11 @@ export default function HomePage() {
      )
 }
 
-function CategoryDay({ label, desc }) {
+function CategoryDay({ label, month, selected, onClick }) {
      return (
-          <button>
-               <span>{desc}</span>
+          <button onClick={onClick} className={`${selected ? `bg-neutral-200 text-neutral-900 border-neutral-200` : `text-neutral-200 border-neutral-400`} cursor-pointer active:scale-95 flex items-center gap-2 border  px-4 py-0.5 rounded-lg`}>
                <span>{label}</span>
+               <span>{month}</span>
           </button>
      )
 }
